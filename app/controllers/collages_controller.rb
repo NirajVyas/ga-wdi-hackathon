e2a8dd5ed7045e8e4fdd0d9d0fa4ebc9881fbcd4
samuel-hill <samuel_hill@me.com>
2014-05-30 09:33:16 +0100

    added if statement to limit pictures

--- app/controllers/collages_controller.rb
+++ app/controllers/collages_controller.rb
@@ -7,19 +7,27 @@ end
 def create
   instagram_api_url = "https://api.instagram.com/v1/users/" + current_user.uid + "/media/recent/?access_token="+ current_user.instagram_token
   @instagram_data = HTTParty.get instagram_api_url
+    
     @instagram_data['data'].each do | image |
-      @collagephoto = Collagephoto.new
-      @collagephoto.image_url = image['images']['standard_resolution']['url']
-      @collagephoto.user_id = current_user.id
-      @collagephoto.save
-    end
+      
+      @image_url = image['images']['standard_resolution']['url']
 
-    redirect_to collages_path
+        if Collagephoto.where(:image_url => "#{@image_url}").blank?
+          @collagephoto = Collagephoto.new
+          @collagephoto.image_url = @image_url
+          @collagephoto.user_id = current_user.id
+          @collagephoto.save
 
-end
+            if Collagephoto.count > 20
+              @last_photo = Collagephoto.first
+              @last_photo.destroy
+            end
+        end
+      
+      end
+      
+      redirect_to collages_path
 
-def show
-  @photos = Collagephoto.find([current_user.id])
-end
+  end
 
 end
